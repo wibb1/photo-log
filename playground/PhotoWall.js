360f48ebe5d3f24625b80290140bd9179ac92921
Will Campbell <wibb1@comcast.net>
2021-04-10 08:52:34 -0400

    move hook versions to playground before redux

---
+++ playground/PhotoWall.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Photo from "./Photo";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+class Photowall extends Component {
+  render() {
+    return (
+      <div>
+        <Link className="add-icon" to="/AddPhoto"></Link>
+        <div className="photo-grid">
+          {this.props.posts
+            .sort(function (x, y) {
+              return y.id - x.id;
+            })
+            .map((post) => (
+              <Photo
+                key={post.id}
+                post={post}
+                onRemovePhoto={this.props.onRemovePhoto}
+              />
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
+Photowall.propTypes = {
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired,
+};
+
+export default Photowall;
